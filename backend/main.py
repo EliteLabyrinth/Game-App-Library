a92da322afed27cde20ef3593e0e07869d3bfe8c
EliteLabyrinth <soumabratabhattacharya@gmail.com>
2022-01-18 23:52:27 +0530

    initial backend is added

---
+++ backend/main.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+app.config.from_object(__name__)
+
+CORS(app, resources={r"/*":{'origins':"*"}})
+# CORS(app, resources={r'/*':{'origins': 'http://localhost:8080',"allow_headers": "Access-Control-Allow-Origin"}})
+
+# hello world route
+@app.route('/', methods=['GET'])
+def greetings():
+    return("Hello, world!")
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
