2907fae1435e949b5db3c94bcdc7ed7b901625cd
EliteLabyrinth <soumabratabhattacharya@gmail.com>
2022-01-19 21:05:20 +0530

    necessary methods to fetch data from backend for hello component have been added

--- frontend/frontend/src/components/Hello.vue
+++ frontend/frontend/src/components/Hello.vue
@@ -1,17 +1,32 @@
 <template>
-    <div>
-        <p> {{msg}} </p>
-    </div>
+  <div>
+    <p>{{ msg }}</p>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
-    name:'Hello',
-    data(){
-        return {
-            msg: "Hello"
-        }
-    }
-
-}
+  name: "Hello",
+  data() {
+    return {
+      msg: "",
+    };
+  },
+  methods: {
+    getResponse() {
+      const path = `http://127.0.0.1:5000/hello`;
+      axios
+        .get(path)
+        .then((res) => {
+          console.log(res.data);
+          this.msg = res.data;
+        })
+        .catch((err) => console.log(err));
+    },
+  },
+  created() {
+    this.getResponse();
+  },
+};
 </script>
