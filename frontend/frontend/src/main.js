7b996e78b3e405cb846dd746a0ac7e2344c49bdb
EliteLabyrinth <soumabratabhattacharya@gmail.com>
2022-01-19 21:34:59 +0530

    code to dynamically load the page title has been added in the main.js file

--- frontend/frontend/src/main.js
+++ frontend/frontend/src/main.js
@@ -9,6 +9,15 @@ import BootstrapVue from "bootstrap-vue";
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
 
+const DEFAULT_TITLE = "Some Default Title";
+router.afterEach((to, from) => {
+  // Use next tick to handle router history correctly
+  // see: https://github.com/vuejs/vue-router/issues/914#issuecomment-384477609
+  Vue.nextTick(() => {
+    document.title = to.meta.title || DEFAULT_TITLE;
+  });
+});
+
 new Vue({
   router,
   render: (h) => h(App),
